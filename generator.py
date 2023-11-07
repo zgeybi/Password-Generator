e3550c72f28aeb90033c1904d0bfcc743c41d411
Tim Zgeybi <128317249+zgeybi@users.noreply.github.com>
2023-11-07 20:39:26 +0300

    Update generator.py
    adde docstring

--- generator.py
+++ generator.py
@@ -35,6 +35,9 @@ def get_punctuation(list_):
 # Main program starts here
 
 def generator():
+    """
+    generates random 16-char password and returns it
+    """
     mylist = []
     punctuation = get_chars(get_punctuation, mylist)
     uppercase = get_chars(get_uppercase, mylist)
